c29a928017fdc1e42d3b372b111db2e65e993c40
Arrivera <andresramirorivera@gmail.com>
2025-03-21 10:37:40 -0700

    routeshash

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,17 @@
-import { Routes } from '@angular/router';
-import { TableComponent } from './table/table.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { TableComponent } from './table/table.component';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
-export const routes: Routes = [
-    {
-        path: '',
-        component: HomeComponent,
-    },
-    {
-        path: 'table',
-        component: TableComponent,
-    }
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'table', component: TableComponent }
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
